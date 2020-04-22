b6c96590713e5a584c8a35d24775ba03bfa5a77b
Javier Colombera <jcolomberea@gmail.com>
2020-04-22 17:25:43 -0300

    Agregado de Idioma Portugues

--- CodingChallenge.Tests/DataTests.cs
+++ CodingChallenge.Tests/DataTests.cs
@@ -104,6 +104,29 @@ namespace CodingChallengeCore.Data.Tests
         }
 
         [TestCase]
+        public void TestResumenListaConMasTiposEnPortugues()
+        {
+            var formas = new List<FormaGeometrica>
+            {
+                new Cuadrado(5),
+                new Circulo(1.5m),
+                new TrianguloEquilatero(4),
+                new Cuadrado(2),
+                new TrianguloEquilatero(9),
+                new Circulo(1.375m),
+                new TrianguloEquilatero(4.2m),
+                new Rectangulo(2, 4),
+                new TrapecioIsosceles(2, 3, 5)
+            };
+
+            var resumen = FormaGeometrica.Imprimir(formas, new Portugues());
+
+            Assert.AreEqual(
+                "<h1>Reporte de Formas</h1>2 Quadrados | Area 29 | Perímetro 28 <br/>2 Círculos | Area 13,01 | Perímetro 18,06 <br/>3 Triângulos | Area 49,64 | Perímetro 51,6 <br/>1 Retângulo | Area 8 | Perímetro 12 <br/>1 Trapézio | Area 6,5 | Perímetro 15,2 <br/>TOTAL:<br/>9 formas Perímetro 124,86 Area 106,15",
+                resumen);
+        }
+
+        [TestCase]
         public void TestResumenListaConUnRectanguloEnCastellano()
         {
             var rectangulo = new List<FormaGeometrica> { new Rectangulo(2, 4) };
